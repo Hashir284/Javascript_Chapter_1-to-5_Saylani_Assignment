f153caedbc1b4a6158dd65e7e3f807934cb49461
Muhammad Hashir <onlyforprofile1@gmail.com>
2025-12-16 21:44:46 +0500

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let obtained Value = prompt("Enter the obtained Value")
-let total Value= prompt("Enter the total Value")
-let percentage = obtained Value / total Value * 100;
+let obtainedValue = prompt("Enter the obtained Value");
+let totalValue= prompt("Enter the total Value");
+let percentage = obtainedValue / totalValue * 100;
 
 alert("Percentage:\n" + percentage);
 
