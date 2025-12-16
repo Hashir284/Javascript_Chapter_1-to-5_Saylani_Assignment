a75661428f4641b63c945ab7d852061773886fa8
Muhammad Hashir <onlyforprofile1@gmail.com>
2025-12-16 23:25:55 +0500

    update

---
+++ c.js
@@ -0,0 +1,5 @@
+        let fahrenhiet = prompt("Enter your Fahrenhiet value");
+        // // alert(fahrenhiet)
+        let celsius = (fahrenhiet -  32)  * (5  /  9)
+        // // alert(fehrenhiet = celsius)
+        alert("Your Celsius Value:\n" + celsius)
