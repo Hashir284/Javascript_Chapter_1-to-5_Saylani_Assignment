f8a0951b0155feaff24c783e35165c9f2a4685f8
Hashir284 <onlyforprofile1@gmail.com>
2025-12-18 22:42:15 +0500

    Add files via upload

---
+++ country.js
@@ -0,0 +1,32 @@
+let CountryName = prompt('Enter your Country name')
+
+if (CountryName == 'Pakistan' || CountryName == 'pakistan') {
+    alert('Pakistan Passport Rank is 7')
+}
+else if (CountryName == 'america' || CountryName == 'America') {
+    alert('Alert! America is terrorist Country and Americian Password Rank is 0')
+}
+else if (CountryName == 'Israel' || CountryName == 'israel') {
+    alert('Alert! Israel is terrorist Country and Israeli Password Rank is 0')
+}
+else if (CountryName == 'Iran' || CountryName == 'iran') {
+    alert('iraq Passport Rank is 6')
+}
+else if (CountryName == 'India' || CountryName == 'Bharat' || CountryName == 'india') {
+    alert('India Passport Rank is 20')
+} 
+else if (CountryName == 'saudiarab' || CountryName == 'Saudiarab' || CountryName == 'saudiarabia') {
+    alert('SauidiArab Passport Rank is 1')
+} 
+else if (CountryName == 'Palestine' || CountryName == 'palestine') {
+    alert('Paletine Passport Rank is 3')
+} 
+else if (CountryName == 'China' || CountryName == 'china') {
+    alert('China Passport Rank is 5')
+} 
+else if (CountryName == 'Iraq' || CountryName == 'iraq') {
+    alert('Iraq Passport Rank is 4')
+} 
+else {
+    alert("Country not found")
+}
