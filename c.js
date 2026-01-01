266c5916db853fbe9d984e275255b48395e2640a
Hashir284 <onlyforprofile1@gmail.com>
2026-01-01 11:00:35 +0500

    Update c.js

--- c.js
+++ c.js
@@ -1,5 +1,17 @@
-        let fahrenhiet = prompt("Enter your Fahrenhiet value");
-        // // alert(fahrenhiet)
-        let celsius = (fahrenhiet -  32)  * (5  /  9)
-        // // alert(fehrenhiet = celsius)
-        alert("Your Celsius Value:\n" + celsius)
+var age = 18;
+alert(age);
+
+var visit = prompt("Please write here! How many times you come on this website visited");
+alert("You have visited this site " + visit + " times.");
+
+var birthYear = 1990;
+alert("My birth year is " + birthYear + "\nData type of my declared variable is number");
+
+var site = "www.xyzClothing.com";
+alert("Welcome to " + site);
+
+var userName = prompt("Please! Enter your name: ");
+var title = prompt("Please! Enter the Order Title: ");
+var quantity = prompt("Please! Enter a product quantity: ");
+
+alert(userName + " ordered " + quantity + " " + title + " on " + site);
